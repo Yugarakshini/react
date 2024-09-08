7b2be8de3aef0bb559928530aeebd94c81c1045a
Yugarakshini <115138657+Yugarakshini@users.noreply.github.com>
2024-09-09 04:53:13 +0530

    Create Message.tsx

---
+++ Message.tsx
@@ -0,0 +1,8 @@
+//PascalCasing followed in react and developers
+function Message() {
+    //JSX: this html code gets converted into js code
+    const name = 'Mosh';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
